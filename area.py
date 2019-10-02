f03c9e83280cbeace2c05a05c42a2b6a8fd1d22a
sanskarg348 <56079398+sanskarg348@users.noreply.github.com>
2019-10-02 19:22:12 +0530

    Add files via upload

---
+++ area.py
@@ -0,0 +1,4 @@
+base=int(input("Enter the base length of the parallelogram: "))
+height=int(input("Enter the height length of the parallelogram: "))
+area=base*height
+print("Area of the parallelogram is: ",area)
